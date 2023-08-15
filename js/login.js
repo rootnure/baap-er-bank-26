a8adaf35546a6e5af4f61be90c36efa5b3e0395f
rootnure <nur.diu.2791@gmail.com>
2023-08-15 19:59:47 +0600

    Login page event handler added

---
+++ js/login.js
@@ -0,0 +1,19 @@
+// step-1: add click event handler with the submit button
+document.querySelector("#btn-login").addEventListener('click', function () {
+    // step-2: get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+    const emailField = document.querySelector("#user-email");
+    const email = emailField.value;
+    // step-3: get password
+    // 3.a: set id on the html element
+    // 3.b: get the element
+    // 3.c: get the value from the element
+    const passwordField = document.querySelector("#user-password");
+    const password = passwordField.value;
+
+    // DANGER: DO NOT VERIFY email password on the client side
+    // step-4: verify email and password and check whether valid user or not
+    if(email === 'pola@baap.com' && password === 'secret') {
+        console.log('all ok');
+    }
+})
